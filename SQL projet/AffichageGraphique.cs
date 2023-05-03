3aead84ef291ec54b2cb456f8fb2f0d06b774daf
Luk <luk.fosse@edu.devinci.fr>
2023-05-03 10:26:38 +0100

    Ajout de AjouterClient et ConnectionClient

--- SQL projet/AffichageGraphique.cs
+++ SQL projet/AffichageGraphique.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -68,6 +71,18 @@ namespace SQL_projet
         {
             Console.Clear();
             Console.WriteLine("Lorem Ipsum");
+            Console.WriteLine("Bienvenu chez Belle Fleure");
+            Console.WriteLine("Que voulez vous faire ?");
+            Console.WriteLine("1. Login");
+            Console.WriteLine("2. Sign up");
+            Console.WriteLine("3. Quiter");
+            int r = GoodValue(1, 3);
+            switch (r)
+            {
+                case 1: ConnectionClient(); break;
+                case 2: AjouterClient(); break;
+                case 3: break;
+            }
             Console.ReadLine();
             Menu();
         }
@@ -92,6 +107,108 @@ namespace SQL_projet
             Console.WriteLine("Lorem Ipsum");
             Console.ReadLine();
             Menu();
+        }
+        static void AjouterClient()
+        {
+            Console.Clear();
+            Console.WriteLine("Prénom : ");
+            string prenom = Console.ReadLine();
+            Console.WriteLine("Nom : ");
+            string nom = Console.ReadLine();
+            Console.WriteLine("Adresse : ");
+            string adresse = Console.ReadLine();
+            Console.WriteLine("Mail : ");
+            string courriel = Console.ReadLine();
+            Console.WriteLine("Mot de passe");
+            string mdp = Console.ReadLine();
+
+            string connectionString = "SERVER=localhost;PORT=3306;DATABASE=Fleurs;UID=root;PASSWORD=root;";
+            int exist = 1;
+            while (exist==1)
+            {
+                using (MySqlConnection connection1 = new MySqlConnection(connectionString))
+                {
+                    connection1.Open();
+
+                    string query = "SELECT EXISTS (SELECT 1 FROM client WHERE courriel = @courriel)";
+                    using (MySqlCommand command1 = new MySqlCommand(query, connection1))
+                    {
+                        command1.Parameters.AddWithValue("@courriel", courriel);
+
+                        exist = Convert.ToInt32(command1.ExecuteScalar());
+                    }
+
+                    connection1.Close();
+                }
+                if (exist != 1)
+                {
+                    Console.WriteLine("Téléphone : ");
+                    int telephone = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(telephone);
+                    Console.WriteLine("Carte de crédit");
+                    string cb = Console.ReadLine();
+                    MySqlConnection connection = new MySqlConnection(connectionString);
+                    connection.Open();
+                    MySqlCommand command = connection.CreateCommand();
+                    command.CommandText = "INSERT INTO `Fleurs`.`client` (`idclient`,`nom`, `prenom`,`telephone`,`courriel`, `motDePasse`, `facturationAdresse`,`creditCard`) VALUES (8,'" + nom + "', '" + prenom + "','" +telephone+ "' ,'" + courriel + "','" + mdp + "', '" + adresse + "',    '" + cb + "');";
+
+                    MySqlDataReader reader;
+                    reader = command.ExecuteReader();
+                    connection.Close();
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Ce mail est déjà associé à un autre client. Veuillez saisir un autre mail et saisir de nouveau le mot de passe");
+                    Console.WriteLine("Mail : ");
+                    courriel = Console.ReadLine();
+                    Console.WriteLine("Mot de passe");
+                    mdp = Console.ReadLine();
+                }
+                break;
+            }
+
+
+        }
+        static void ConnectionClient() 
+        {
+            Console.Clear();
+            int exist = 0;
+            while (exist == 0) 
+            {
+                Console.WriteLine("Courriel :");
+                string courriel = Console.ReadLine();
+                Console.WriteLine("Mot de passe :");
+                string mdp = Console.ReadLine();
+                string connectionString = "SERVER=localhost;PORT=3306;DATABASE=Fleurs;UID=root;PASSWORD=root;";
+                using (MySqlConnection connection1 = new MySqlConnection(connectionString))
+                {
+                    connection1.Open();
+
+                    string query = "SELECT EXISTS (SELECT 1 FROM client WHERE courriel = @courriel)";
+                    using (MySqlCommand command1 = new MySqlCommand(query, connection1))
+                    {
+                        command1.Parameters.AddWithValue("@courriel", courriel);
+
+                        exist = Convert.ToInt32(command1.ExecuteScalar());
+                    }
+
+                    connection1.Close();
+                }
+                if(exist == 0) 
+                {
+                    Console.Clear();
+                    Console.WriteLine("Courrier ou mot de passe éronné. Veuillez réessayer");
+                }
+                else 
+                {
+                    Console.Clear();
+                    Console.WriteLine("Bienvenue");
+                }
+
+            }
+            
+
         }
     }
 }
--- SQL projet/Program.cs
+++ SQL projet/Program.cs
@@ -5,7 +5,7 @@ namespace SQL_projet
     {
         static void Main(string[] args)
         {
-            string login = ""; //login pour se connecter à la database
+            string login = "SERVER=localhost;PORT=3306;DATABASE=Fleurs;UID=root;PASSWORD=root;"; //login pour se connecter à la database
             // Sql_fetcher fetcher = new Sql_fetcher(login);
             AffichageGraphique affichage = new AffichageGraphique();
             affichage.Affichage();
